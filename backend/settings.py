e7ef21708baf1b0dcb1ad75adbe9386f0b8271bb
subhikshya shretha <subhikshyashrestha@gmail.com>
2026-05-24 13:59:49 +0545

    added PostgreSQL and mood tracking

--- backend/settings.py
+++ backend/settings.py
@@ -74,11 +74,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+import os
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'
+    )
 }
 
 
--- journal/models.py
+++ journal/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 class JournalEntry(models.Model):
+    MOOD_CHOICES = [
+        ('happy', 'Happy'),
+        ('sad', 'Sad'),
+        ('productive', 'Productive'),
+    ]
     title = models.CharField(max_length=200)
     content = models.TextField()
+    mood = models.CharField(max_length=20, choices=MOOD_CHOICES, default='happy')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
